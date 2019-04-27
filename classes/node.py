5187ce3ec1f67798a54a5f7bfbf761a5cdae1677
KKosukeee <kousuke.newlife@gmail.com>
2019-04-27 16:09:53 +0900

    Created Node object for linked-list implementation

---
+++ classes/node.py
@@ -0,0 +1,17 @@
+"""
+An implementation of node for a linked-list
+"""
+
+class Node:
+    """
+    Node class implementation
+    """
+
+    def __init__(self, value):
+        """
+        A value for this particular node
+        Args:
+            value: integer
+        """
+        self.value = value
+        self.next = None
