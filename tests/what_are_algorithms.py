fbddb9af7a0251fa9a9693d700d56d4bc19d447b
KKosukeee <kousuke.newlife@gmail.com>
2019-05-11 12:24:05 +0900

    Created test cases for what_are_algorithms.py file

--- tests/what_are_algorithms.py
+++ tests/what_are_algorithms.py
@@ -1,27 +1,36 @@
 """
-    Unit-test for functions within what_are_algorithms.py file
+Unit-test for functions within what_are_algorithms.py file
 """
 
 from unittest import TestCase
+from part1.what_are_algorithms import naive_approach
+from part1.what_are_algorithms import optimal_approach
 
-class TestNaiveApproach(TestCase):
+class TestWhatAreAlgorithms(TestCase):
     """
-        Test case for naive_approach function
+    Test case for both naive and optimal approach
     """
-
     def setUp(self):
-        pass
+        self.cases = (
+            ([2, 7, 11, 15], 9, [0, 1]),
+            ([3, 1, 4, 11], 14, [0, 3]),
+            ([5, 1, 9, 2], 12, []),
+        )
 
-    def tearDown(self):
-        pass
+    def test_naive_approach(self):
+        """
+        Tests two sum problem with naive function
+        Returns:
 
-class TestOptimalApproach(TestCase):
-    """
-        Test case for optimal_approach function
-    """
+        """
+        for nums, target, answer in self.cases:
+            self.assertListEqual(naive_approach(nums, target), answer)
 
-    def setUp(self):
-        pass
+    def test_optimal_approach(self):
+        """
+        Tests two-sum problem with optimal function
+        Returns:
 
-    def tearDown(self):
-        pass
+        """
+        for nums, target, answer in self.cases:
+            self.assertListEqual(optimal_approach(nums, target), answer)
